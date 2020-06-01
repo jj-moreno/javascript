7edcb9a9f0299004b02a30e0a50ba583f7946585
unknown <moreno7046@hotmail.com>
2020-06-01 10:25:36 -0500

    finished exercise 4

--- dom/shoppingList.js
+++ dom/shoppingList.js
@@ -1,41 +1,63 @@
 /*
-    Do the following steps in this file, and make sure this file is linked to the shopping-list.html file
+Do the following steps in this file, and make sure this file is linked to the shopping-list.html file
 
 
-    1. Create three variables that hold references to the following elements &lt;ul&gt; &lt;input&gt; &lt;button&gt; elements.
-
-    
-    2. Create a function that will run in response to the button being clicked.
-
- 
-
-    3. Inside the function body, start off by storing the current value of the input element in a variable.
+ 1. Create three variables that hold references to the following elements &lt;ul&gt; &lt;input&gt; &lt;button&gt; elements.
 
 
-
-    4. Next, empty the input element by setting its value to an empty string — ''.
-
-   
-
-    5. Create three new elements — a list item (&lt;li&gt;), &lt;span&gt;, and &lt;button&gt;, and store them in variables.
-
-    
-
-    6. Append the span and the button as children of the list item.
-
-    
-    
-
-    7. Set the text content of the span to the input element value you saved earlier, and the text content of the button to 'Delete'.
-
-    
-
-    8. Append the list item as a child of the list.
+ 2. Create a function that will run in response to the button being clicked.
 
 
+ 3. Inside the function body, start off by storing the current value of the input element in a variable.
 
-    9. Attach an event handler to the delete button, so that when clicked it will delete the entire list item it is inside.
 
-    
+ 4. Next, empty the input element by setting its value to an empty string — ''.
 
-    10. Finally, use the focus() method to focus the input element ready for entering the next shopping list item.
+
+ 5. Create three new elements — a list item (&lt;li&gt;), &lt;span&gt;, and &lt;button&gt;, and store them in variables.
+
+
+ 6. Append the span and the button as children of the list item.
+
+
+ 7. Set the text content of the span to the input element value you saved earlier, and the text content of the button to 'Delete'.
+
+
+ 8. Append the list item as a child of the list.
+
+
+ 9. Attach an event handler to the delete button, so that when clicked it will delete the entire list item it is inside.
+
+
+ 10. Finally, use the focus() method to focus the input element ready for entering the next shopping list item.
+*/
+
+
+const ul = document.querySelector("ul");
+const input = document.querySelector("input");
+const btn = document.querySelector("button");
+
+function btnClicked() {
+    const current = input.value;
+    input.value = '';
+
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    const delBtn = document.createElement("button");
+
+    li.appendChild(span);
+    li.appendChild(delBtn);
+
+    span.textContent = current;
+    delBtn.textContent = 'delete';
+
+    ul.appendChild(li);
+
+    delBtn.addEventListener('click', () => {
+        li.remove();
+        input.focus();
+    });
+
+}
+
+btn.addEventListener('click', btnClicked);
--- dom/timer/timer-dom.js
+++ dom/timer/timer-dom.js
@@ -1,2 +1,57 @@
 console.log("time to start the timer!");
 
+const timer = document.querySelector('p');
+let timeInSecs = 0;
+let counter = 0;
+
+const interval = setInterval(startTimer, 1000);
+
+function convertTime(seconds) {
+    const min = Math.floor(seconds / 60);
+    const sec = seconds % 60;
+    return `${min}:${sec}`;
+}
+
+function startTimer() {
+
+    updateTime();    
+}
+
+
+function updateTime() {
+    counter++;
+    timer.textContent = `${convertTime(timeInSecs - counter)}`;
+
+    if (counter === timeInSecs) {
+        clearInterval(interval);
+    }
+}
+
+function stopTimer() {
+    timer.textContent = '0:00';
+    clearInterval(interval);
+}
+
+function resetTimer() {
+    timer.textContent = `${timeInSecs / 60}:00`;
+    counter = 0;
+}
+
+function setEventListeners() {
+    document.querySelector("#start").addEventListener('click', startTimer);
+    document.querySelector("#stop").addEventListener('click', stopTimer);
+    document.querySelector("#reset").addEventListener('click', resetTimer);
+}
+
+function init() {
+    setEventListeners();
+    const numOfMins = prompt('How many minutes would you like to set for your timer?');
+    if(parseInt(numOfMins)) {
+        timeInSecs = numOfMins * 60;
+        timer.textContent = `${numOfMins}:00`;
+    } else {
+        alert('Invalid number!');
+    }
+}
+
+init();
