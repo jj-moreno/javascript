0b59746ea7125ffed031fcb684ae3ec352797ccd
unknown <moreno7046@hotmail.com>
2020-12-11 15:28:29 -0600

    finished, formatting and bug fixes for start, stop, reset btns

--- dom/timer/timer-dom.js
+++ dom/timer/timer-dom.js
@@ -1,8 +1,8 @@
 const timer = document.querySelector('p');
 let timerTotalMins;
-let timeRemainingInSecs;
 let interval;
 let secsPassed = 0;
+let timeRemainingInSecs;
 
 const getTimerTotalSecs = () => {
   return timerTotalMins * 60;
@@ -13,23 +13,38 @@ const getTimeRemainingInSecs = (secsPassed) => {
 };
 
 const start = () => {
-  interval = setInterval(() => {
-    secsPassed += 1;
-    timeRemainingInSecs = getTimeRemainingInSecs(secsPassed);
-    timer.textContent = timeRemainingInSecs;
-    if (timeRemainingInSecs < 0) {
-      stop(interval);
-    }
-  }, 1000);
+  if (!interval) {
+    interval = setInterval(() => {
+      secsPassed += 1;
+      timeRemainingInSecs = getTimeRemainingInSecs(secsPassed);
+      updateTime(timeRemainingInSecs);
+      if (timeRemainingInSecs == 0) {
+        stop(interval);
+      }
+    }, 1000);
+  }
+};
+
+const updateTime = (timeRemainingInSecs) => {
+  const mins = Math.floor(timeRemainingInSecs / 60);
+  const secs = timeRemainingInSecs % 60;
+  timer.textContent = `${
+    mins < 10 ? prefixSingleDigitNumWithZero(mins) : mins
+  }:${secs < 10 ? prefixSingleDigitNumWithZero(secs) : secs}`;
+};
+
+const prefixSingleDigitNumWithZero = (num) => {
+  return `0${num}`;
 };
 
 const stop = () => {
   clearInterval(interval);
+  interval = null;
 };
 
 const reset = () => {
   secsPassed = 0;
-  timer.textContent = timerTotalMins;
+  updateTime(getTimeRemainingInSecs(secsPassed));
 };
 
 const addTimerButtonListeners = () => {
@@ -42,7 +57,8 @@ const init = () => {
   while (!parseInt(timerTotalMins)) {
     timerTotalMins = prompt('how many minutes for timer?');
   }
-  timer.textContent = timerTotalMins;
+
+  updateTime(getTimeRemainingInSecs(secsPassed));
   addTimerButtonListeners();
 };
 
