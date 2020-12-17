320459e3970c4af33d4378b79e01922b18f8c6ce
unknown <moreno7046@hotmail.com>
2020-12-17 10:58:07 -0600

    removed console log for mins and secs

--- dom/timer/timer-dom.js
+++ dom/timer/timer-dom.js
@@ -1,34 +1,70 @@
-const timer = document.querySelector('p');
-let timerTotalMins;
-let interval;
-let secsPassed = 0;
-let timeRemainingInSecs;
-
-const getTimerTotalSecs = () => {
-  return timerTotalMins * 60;
+const getUrlParams = () => {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const timerMins = parseInt(urlParams.get('min'));
+  const timerSecs = parseInt(urlParams.get('sec'));
+  return {
+    mins: timerMins,
+    secs: timerSecs,
+  };
 };
 
-const getTimeRemainingInSecs = (secsPassed) => {
-  return getTimerTotalSecs() - secsPassed;
+const getTotalSecs = (mins, secs) => {
+  return mins * 60 + secs;
+};
+
+const getTimer = () => {
+  return document.querySelector('#timer-display').textContent;
+};
+
+const getTimeRemainingInSecs = () => {
+  const timer = getTimer();
+  const splitTimer = timer.split(':');
+  const mins = parseInt(splitTimer[0]);
+  const secs = parseInt(splitTimer[1]);
+  return getTotalSecs(mins, secs);
 };
 
 const start = () => {
-  if (!interval) {
-    interval = setInterval(() => {
-      secsPassed += 1;
-      timeRemainingInSecs = getTimeRemainingInSecs(secsPassed);
-      updateTime(timeRemainingInSecs);
-      if (timeRemainingInSecs == 0) {
-        stop(interval);
-      }
-    }, 1000);
-  }
+  const timeRemainingInSecs = getTimeRemainingInSecs();
+  const timer = interval(timeRemainingInSecs);
+  addStopButtonListener(timer);
+};
+
+const interval = (timerTotalSecs) => {
+  let timeRemainingInSecs = timerTotalSecs;
+  return setInterval(() => {
+    timeRemainingInSecs -= 1;
+    updateTime(timeRemainingInSecs);
+  }, 1000);
+};
+
+const addStopButtonListener = (timer) => {
+  document
+    .getElementById('stop')
+    .addEventListener('click', stop.bind(null, timer));
+};
+
+const stop = (interval) => {
+  clearInterval(interval);
+};
+
+const reset = () => {
+  updateTime(getTotalSecs());
 };
 
 const updateTime = (timeRemainingInSecs) => {
-  const mins = Math.floor(timeRemainingInSecs / 60);
-  const secs = timeRemainingInSecs % 60;
-  timer.textContent = `${
+  if (timeRemainingInSecs >= 0) {
+    const mins = Math.floor(timeRemainingInSecs / 60);
+    const secs = timeRemainingInSecs % 60;
+    formatTime(mins, secs);
+  } else {
+    clearInterval(this);
+  }
+};
+
+const formatTime = (mins, secs) => {
+  document.querySelector('p').textContent = `${
     mins < 10 ? prefixSingleDigitNumWithZero(mins) : mins
   }:${secs < 10 ? prefixSingleDigitNumWithZero(secs) : secs}`;
 };
@@ -37,29 +73,22 @@ const prefixSingleDigitNumWithZero = (num) => {
   return `0${num}`;
 };
 
-const stop = () => {
-  clearInterval(interval);
-  interval = null;
-};
-
-const reset = () => {
-  secsPassed = 0;
-  updateTime(getTimeRemainingInSecs(secsPassed));
-};
-
-const addTimerButtonListeners = () => {
-  document.getElementById('start').addEventListener('click', start);
-  document.getElementById('stop').addEventListener('click', stop);
-  document.getElementById('reset').addEventListener('click', reset);
-};
-
 const init = () => {
-  while (!parseInt(timerTotalMins)) {
-    timerTotalMins = prompt('how many minutes for timer?');
-  }
+  const urlParams = getUrlParams();
+  const timerTotalSecs = getTotalSecs(urlParams['mins'], urlParams['secs']);
+  updateTime(timerTotalSecs);
+  addStartButtonListener();
+  addResetButtonListener(timerTotalSecs);
+};
 
-  updateTime(getTimeRemainingInSecs(secsPassed));
-  addTimerButtonListeners();
+const addStartButtonListener = () => {
+  document.getElementById('start').addEventListener('click', start);
+};
+
+const addResetButtonListener = (timerTotalSecs) => {
+  document
+    .getElementById('reset')
+    .addEventListener('click', reset.bind(null, timerTotalSecs));
 };
 
 init();
